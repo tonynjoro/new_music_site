e976ce286a264679a6a3f6d44427b7c7918058eb
kosh <kosh@localhost.localdomain>
2024-05-19 11:13:13 +0300

    prepared for production

--- music/music/settings.py
+++ music/music/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -69,6 +70,8 @@ TEMPLATES = [
         },
     },
 ]
+
+
 
 WSGI_APPLICATION = 'music.wsgi.application'
 
@@ -120,6 +123,18 @@ USE_TZ = True
 STATIC_ROOT =  os.path.join(BASE_DIR,'music_site/static')
 
 STATIC_URL = 'static/'
+
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+    },
+    "mediafiles": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+}
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
